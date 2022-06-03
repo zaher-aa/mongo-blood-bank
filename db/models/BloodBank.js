14c90c32040e501aa3f51abab7b9be9f1cfdd6f7
zaher-aa <zaherabuamro@gmail.com>
2022-06-03 16:24:49 +0300

    added bloodband model

---
+++ db/models/BloodBank.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const bloodBankSchema = new Schema({
+  name: {
+    type: 'string',
+    required: [true, 'donor name is required'],
+    trim: true,
+  },
+  city: {
+    type: 'string',
+    required: [true, 'city name is required'],
+    trim: true,
+  },
+  contactNumber: {
+    type: 'string',
+    required: true,
+    trim: true,
+  },
+  donors: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Donor',
+    },
+  ],
+});
+
+module.exports = model('BloodBank', bloodBankSchema);
